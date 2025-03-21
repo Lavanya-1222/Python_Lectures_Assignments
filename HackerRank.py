bb2911b0131d9f1c941c9bdb156ffa53f2522bd4
Lavanya-1222 <lavanyan1205@gmail.com>
2025-03-21 18:03:07 +0530

    Hacker rank done

--- HackerRank.py
+++ HackerRank.py
@@ -49,7 +49,7 @@ if(True):
 # print(len(l))
 
 # Counting_Sort
-""" """
+""" 
 
 def countingSort(arr):
     # Write your code here
@@ -61,4 +61,71 @@ def countingSort(arr):
     for j in arr:
         l[j]+=1
     return l
-print(countingSort([1,1,3,2,1]))
+print(countingSort([1,1,3,2,1]))"
+"""
+
+# s=input("enter string ").lower()
+# print(s)
+# sealevel=0
+# start=s[0]
+# valley=0
+# for i in range(len(s)):
+#     if s[i]=='u':
+#         sealevel+=1
+#     if s[i]=='d':
+#         sealevel-=1
+#     if start=='u' and sealevel==0 and i!=0:
+#         start=s[i+1]
+#         sealevel=0
+#     if start=='d' and sealevel==0 and i!=0:
+#         start=s[i+1]
+#         sealevel=0
+#         valley+=1
+
+# print(valley)
+
+# String Pangram or not 
+# import string
+# s=input("enter string ").lower()
+# print(string.ascii_letters)
+# d=len({i:1 for i in s if i in string.ascii_letters})
+# if d==26:
+#     print("Pangram")
+# else:
+#     print("Not")
+
+
+# Mars Exploration
+# s='sossot'
+# c=0
+# for i in range(0,len(s),3):
+#     sp=s[i:i+3]
+
+#     if sp!='sos':
+#         if sp[0]!='s':
+#             c+=1
+#         if sp[1]!='o':
+#             c+=1
+#         if sp[2]!='s':
+#             c+=1
+
+# print(c)
+
+
+# Permutation Two Array
+a=[0,1]
+b=[0,2]
+
+a.sort()
+print(a)
+b.sort(reverse=True)
+print(b)
+k=1
+f=1
+for i in range(len(a)):
+    if k>(a[i]+b[i]):
+        f=0
+if f==1:
+    print("Yes")
+else:
+    print("No")
